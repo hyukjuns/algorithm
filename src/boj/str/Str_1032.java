f86732f979bec2ae61c6df6189e696309d954f2f
namhj94 <hyukjun1994@gmail.com>
2019-10-19 17:29:31 +0900

    str

---
+++ src/boj/str/Str_1032.java
@@ -0,0 +1,33 @@
+package boj.str;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Str_1032 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        String[] str = new String[n];
+        for(int i=0; i<n; i++){
+            str[i] = br.readLine();
+        }
+        String temp = str[0];
+
+        for(int i=1; i<n; i++){
+            StringBuilder res = new StringBuilder();
+            for(int j=0; j<temp.length(); j++){
+                if(temp.charAt(j) == str[i].charAt(j))
+                    res.append(temp.charAt(j));
+                else
+                    res.append('?');
+            }
+            temp = res.toString();
+        }
+        bw.write(temp);
+        bw.flush();
+        bw.close();
+    }
+}
