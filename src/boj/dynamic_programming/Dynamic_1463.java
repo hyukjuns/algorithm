887320a6b5ac17029c06a8b2d9f2c6fcfc0b78c8
namhj94 <hyukjun1994@gmail.com>
2019-10-29 21:12:10 +0900

    Dynamic_programming

---
+++ src/boj/dynamic_programming/Dynamic_1463.java
@@ -0,0 +1,30 @@
+package boj.dynamic_programming;
+
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Dynamic_1463 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n =  Integer.parseInt(br.readLine());
+        int[] numArr = new int[1000001];
+        numArr[1] = 0;
+        numArr[2] = numArr[3] = 1;
+        for(int i=4; i<=n; i++){
+            numArr[i] = numArr[i-1]+1;
+            if(i % 3 == 0){
+                numArr[i] = Math.min(numArr[i],numArr[i/3]+1);
+            }
+            if(i % 2 == 0){
+                numArr[i] = Math.min(numArr[i],numArr[i/2]+1);
+            }
+        }
+        bw.write(numArr[n]+"");
+        bw.flush();
+        bw.close();
+    }
+}
