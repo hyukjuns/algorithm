c280d8ae78eaef4e1e1d2ec237ddbfba97f24aa9
namhj94 <hyukjun1994@gmail.com>
2019-12-31 20:31:28 +0900

    dp

--- src/boj/bruteforce/bruteforce_14501.java
+++ src/boj/bruteforce/bruteforce_14501.java
@@ -12,21 +12,18 @@ public class bruteforce_14501 {
         int[] t = new int[n+1];
         int[] p = new int[n+1];
         int[] dp = new int[n+1];
-        int tsum, psum;
         for(int i=0; i<n; i++){
             st = new StringTokenizer(br.readLine());
             t[i] = Integer.parseInt(st.nextToken());
             p[i] = Integer.parseInt(st.nextToken());
         }
 
-        int max = Integer.MIN_VALUE;
         for(int i=0; i<n; i++){
             dp[i+1] = Math.max(dp[i],dp[i+1]);
             if(i+t[i] <= n)
                 dp[i+t[i]] = Math.max(dp[i+t[i]], dp[i] + p[i]);
         }
         bw.write(dp[n]+"");
-        bw.write("dp5: "+dp[6]);
         bw.flush();
         bw.close();
 
