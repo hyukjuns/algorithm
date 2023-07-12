34a308888e41c225d95d06156d36e9f38837171c
hyukjun <hyukjuner@gmail.com>
2023-07-12 21:12:20 +0900

    update python

---
+++ python/practice/test.py
@@ -0,0 +1,5 @@
+from test_class.mod1 import MyClass
+
+a = MyClass(1,2)
+
+print(a.add())
---
+++ python/practice/test_class/mod1.py
@@ -0,0 +1,11 @@
+VAL = 11
+
+class MyClass:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    def add(self):
+        return self.a + self.b
+    
+def sub(a, b):
+    return a-b
