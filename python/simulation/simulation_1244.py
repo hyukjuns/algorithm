b2af54759271db95546ae8a5454d2fc53496529a
hyukjun <hyukjuner@gmail.com>
2023-08-17 22:00:24 +0900

    update simul

--- python/simulation/simulation_1244.py
+++ python/simulation/simulation_1244.py
@@ -5,12 +5,12 @@ def solution():
     switches = list(map(int, sys.stdin.readline().split()))
     students = int(sys.stdin.readline())
     
-    for i in range(0,students):
+    for _ in range(0,students):
         g, num = map(int, sys.stdin.readline().split())
         # Male
         if g == 1:
             for j in range (1,switch_len+1):
-                if j / num == 0:
+                if j % num == 0:
                     switches[j-1] ^= 1
         # Female
         else:
@@ -18,6 +18,9 @@ def solution():
             while True:
                 l = num - j
                 r = num + j
+                if l == 0 or r == switch_len+1:
+                    switches[num-1] ^= 1
+                    break
                 if switches[l-1] == switches[r-1]:
                     switches[l-1] ^= 1
                     switches[r-1] ^= 1
@@ -26,6 +29,15 @@ def solution():
                     switches[num-1] ^= 1
                     break
     return switches
+
 if __name__ == "__main__":
     answer = solution()
-    print(answer)
+    list_len = len(answer)
+    cnt = 0
+    for i in range (0, list_len):
+        print(answer[i], end='')
+        if (i+1) % 20 == 0:
+            print()
+        else:
+            print(' ', end='')
+        cnt += 1
